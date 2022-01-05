716de567c21bfa9293eb4f4aa0b45e4eb736f567
Krismina <krismina.la@gmail.com>
2022-01-05 07:45:59 +0000

    initial commit with all my work so far

---
+++ if-statement.js
@@ -0,0 +1,10 @@
+let fruit = 'orange';
+let x;
+
+if (fruit.length > 5) {
+    x = 'The fruit name has more than five characters.';
+} else {
+    x = 'The fruit name has five characters or less.';
+}
+
+console.log(x);
---
+++ rounding-numbers.js
@@ -0,0 +1,3 @@
+let roundUp = 1.5;
+let x = Math.round(roundUp);
+console.log(x);
